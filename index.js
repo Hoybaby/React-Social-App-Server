f4d5d51629318bf51e3c3d367940836c2b368c5e
Hoybaby <mbartek436@gmail.com>
2021-09-24 21:28:57 -0400

    fixing some things maybe

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
-const { ApolloServer, PubSub} = require('apollo-server');
-
-// Database connection
+const { ApolloServer} = require('apollo-server');
 const mongoose = require('mongoose');
 
-// typedDefs needed for GraphQL and Resolvers for the trypedDefs
 const typeDefs = require('./graphql/typedDefs')
 const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
 
-
-const pubsub = new PubSub();
-
-// connection to database
-const {MONGODB} = require('./config.js')
-
-// what do resolvers do? For each quiery, mutation, subscription, it has it corresponding resolver
-// if we have a query of sayHi, it needs a resolver of sayHi
+// const pubsub = new PubSub();
 
 const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // the third arguement is the apollo 
-    context: ({req}) => ({req, pubsub}),
-    
-
-
+    context: ({ req }) => ({ req })
 });
 
+ const db_password = process.env.DB_PASSWORD
 
-
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true } )
+mongoose
+    .connect(`mongodb+srv://hoybaby:Welcome0@firstcluster.b4kva.mongodb.net/Mern?retryWrites=true&w=majority`, { useNewUrlParser: true })
     .then(() => {
-        console.log('MONGODB is connected')
-        return server.listen({ port: PORT})
-    }).then(res => {
-        console.log(`server running at ${res.url}`);
-    }).catch(err => {
-        console.log(err)
-    
+        console.log('MongoDB Connected');
+        return server.listen({ port: PORT });
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.error(err)
 })
